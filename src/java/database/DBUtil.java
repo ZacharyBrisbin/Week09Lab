ffabeec597d04620f6d6b2e36291f6b9100943e1
Zachary <696019@SAIT221356IT.ACDM.DS.SAIT.CA>
2020-11-10 15:14:56 -0700

    fixed things but not sure if this will be accepted since its after due date

--- src/java/database/DBUtil.java
+++ src/java/database/DBUtil.java
@@ -6,7 +6,7 @@ import javax.persistence.Persistence;
 
 public class DBUtil {
     private static final EntityManagerFactory emf =
-    Persistence.createEntityManagerFactory("NotesPU");
+    Persistence.createEntityManagerFactory("UserPU");
  
     public static EntityManagerFactory getEmFactory() {
         return emf;
